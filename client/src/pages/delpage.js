05bcf8523047a99eda3f7d51ee550c5936941559
Abhinav Sharma <abhinav291sharma@gmail.com>
2022-12-22 01:04:51 +0530

    Changes to client folder

--- client/src/pages/delpage.js
+++ client/src/pages/delpage.js
@@ -1,8 +1,24 @@
 //Draft this page after a while
 import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-export const delpage = () => {
-    
+export const delpage = (id) => {
+    const navigate = useNavigate();
+
+    const handleDelete = async(id) => {
+        e.preventDefault();
+        axios.delete('http://localhost:5000/api/blogs/del', {
+            id: id,
+        })
+        .then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
+        });
+
+        navigate("/");
+    }
 }
 
 export default delpage;
