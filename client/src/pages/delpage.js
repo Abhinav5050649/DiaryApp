59fd0020899ffdd3083eefcdb8d4369eedd73a9a
Abhinav Sharma <abhinav291sharma@gmail.com>
2022-12-22 12:18:16 +0530

    Changes to client folder

--- client/src/pages/delpage.js
+++ client/src/pages/delpage.js
@@ -6,19 +6,47 @@ import { useNavigate } from 'react-router-dom';
 export const delpage = (id) => {
     const navigate = useNavigate();
 
-    const handleDelete = async(id) => {
-        e.preventDefault();
-        axios.delete('http://localhost:5000/api/blogs/del', {
-            id: id,
-        })
-        .then((response) => {
-            console.log(response);
-        }, (error) => {
-            console.log(error);
-        });
+    const [data, setData] = useState([])
 
-        navigate("/");
+    React.useEffect(() => {
+        const getData = async() => {
+            const response = await axios.get('https://localhost:5000/api/blogs/gt')
+                setData(response.data)
+            }
+            getData()
+    }, [])
+
+    const handleDelete = (id) => {
+        axios 
+            .delete(`/api/todos/${id}`)
+            .then((res) => {
+                if (res.data)
+                {
+                    navigate("/");
+                }
+            })
+            .catch((err) => console.log(err));        
     }
+
+    return(
+        <>
+            <ul>
+                {data && data.length > 0 ? (
+                    data.map((item) => {
+                        return (
+                            <li key={item._id} onClick = {() => handleDelete(item._id)}>
+                                {item.blogDate}
+                                <br/>
+                                {item.blogContent}
+                            </li>
+                        );
+                    })
+                ) : (
+                    <li>No Blog Posts left</li>
+                )}
+            </ul>
+        </>
+    )
 }
 
 export default delpage;
